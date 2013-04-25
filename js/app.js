a692fcf388c47cb01126d31067bf0f9c45d909c2
David Gil <david.gil@biko2.com>
2013-04-25 20:14:19 +0200

    Ajustados el localscroll

--- js/app.js
+++ js/app.js
@@ -16,4 +16,8 @@
 
   $('#navbarque').scrollspy();
 
+  $(".scroll").click(function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top - 50}, 500);
+  });
 }());
