408d1f3a879cd3d086ce0ff7e4f0442d09b4e579
David Gil <david.gil@biko2.com>
2013-05-13 02:53:31 -0700

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@
 
   // iluminación del menu de que-es con scrollspy
   $('body').scrollspy({
-      offset: $('#navbarque').height() + 5
+      offset: $('#navbarque').height() - 10
   });
 
   //sistema de smooth scroll para anclas
