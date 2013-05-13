49f06318ccf63a6e4b10da374a39a4084437fc5b
David Gil <david.gil@biko2.com>
2013-05-13 02:48:06 -0700

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@
 
   $('#navbarque-wrapper').height($("#navbarque").height());
   $('#navbarque').affix({
-      offset: $('#navbarque').position() + 10
+      offset: $('#navbarque').position() - 10
   });
 
   // iluminación del menu de que-es con scrollspy
